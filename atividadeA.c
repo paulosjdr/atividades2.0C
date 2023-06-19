2e1fb3b47dbfebe48e78a425065ef7a50afa705a
paulosjdr <134538834+paulosjdr@users.noreply.github.com>
2023-06-19 15:45:53 -0300

    Create atividadeA.c

---
+++ atividadeA.c
@@ -0,0 +1,25 @@
+//Elaborar um programa que apresente como resultado os quadrados dos números inteiros existentes na faixa de valores de 15 a 200.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    int i;
+
+    printf("Os números inteiros de 15 a 200 elevados ao quadrado:\n\n");
+
+    //fazer a leitura do número 15, menor ou igual a 200
+    for (i = 15; i <= 200; i++) {
+        //fazer a operação do quadrado dos números    
+        int quadrado = i * i;
+        //mostrar o resultado do quadrado dos números
+        printf("%d^2 = %d\n", i, quadrado);
+    }
+
+    return 0;
+}
+
