266f5de36150d0b7c8f46f0f8ad1d4569b1e2c87
paulosjdr <134538834+paulosjdr@users.noreply.github.com>
2023-06-19 16:20:01 -0300

    Update atividadeD.c

--- atividadeD.c
+++ atividadeD.c
@@ -1,9 +1,11 @@
-// Elaborar um programa que apresente todos os valores numéricos inteiros ímpares 
+/*
+Elaborar um programa que apresente todos os valores numéricos inteiros ímpares 
 situados na faixa de O a 20. Sugestão: para verificar se o valor numérico é ímpar,
 dentro do laço de repetição, fazer a verificação lógica dessa condição com a instrução if
 dentro do próprio laço, perguntando se o valor numérico do contador é ímpar (se o resto
 do número dividido por 2 é diferente de zero); sendo, mostre-o; não sendo, passe para
 o próximo valor numérico.
+*/
   
 #include <stdio.h>
 #include <stdlib.h>
